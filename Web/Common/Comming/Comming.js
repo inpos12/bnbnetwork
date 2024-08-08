235e148e45e745dc139691d424a04f7adf254b0c
inpos12 <inpos_@naver.com>
2024-08-08 11:53:45 +0900

    Last version2

--- Web/Common/Comming/Comming.js
+++ Web/Common/Comming/Comming.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  $.get("./Web/common/Nav/nav.html", function (data) {
+  $.get("../../common/Nav/nav.html", function (data) {
     $("nav").html(data);
   });
-  $.get("./Web/Common/Comming/Comming.html", function (data) {
+  $.get("../../Common/Comming/Comming.html", function (data) {
     $("#Comming").html(data);
   });
-  $.get("./Web/Common/Footer/Footer.html", function (data) {
+  $.get("../../Common/Footer/Footer.html", function (data) {
     $("footer").html(data);
   });
 });
