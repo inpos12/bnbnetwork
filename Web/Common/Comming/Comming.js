7fd4f3c87eece7251c8fcc2f185e64d3287491f5
inpos12 <inpos_@naver.com>
2024-08-08 12:44:48 +0900

    Last version2

--- Web/Common/Comming/Comming.js
+++ Web/Common/Comming/Comming.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  $.get("/bnbnetwrok/Web/Common/Nav/nav.html", function (data) {
+  $.get("/bnbnetwork/Web/Common/Nav/nav.html", function (data) {
     $("nav").html(data);
   });
-  $.get("/bnbnetwrok/Web/Common/Comming/Comming.html", function (data) {
+  $.get("/bnbnetwork/Web/Common/Comming/Comming.html", function (data) {
     $("#Comming").html(data);
   });
-  $.get("/bnbnetwrok/Web/Common/Footer/Footer.html", function (data) {
+  $.get("/bnbnetwork/Web/Common/Footer/Footer.html", function (data) {
     $("footer").html(data);
   });
 });
